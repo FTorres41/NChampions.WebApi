de021cb565e6ccaaf5d60704539620dd11b1a4e9
Fabio Torres <fabio.torres.41@gmail.com>
2021-01-17 12:19:35 -0300

    Adiciona Handler e CustomConfiguration para Startup

---
+++ NChampions.Application/Handlers/TeamHandler.cs
@@ -0,0 +1,19 @@
+﻿using NChampions.Domain.Commands.Teams;
+using NChampions.Domain.Handlers.Interfaces;
+using NChampions.Domain.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NChampions.Application.Handlers
+{
+    public class TeamHandler : ITeamHandler
+    {
+        public Task<ApiResponse> Handle(CreateTeamCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NChampions.WebApi/Configurations/ServicesConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NChampions.WebApi.Configurations
+{
+    public static class ServicesConfigurations
+    {
+        public static IServiceCollection AddCustomConfiguration(this IServiceCollection services)
+        {
+            var assembly = AppDomain.CurrentDomain.Load("NChampions.Application");
+            services.AddMediatR(assembly);
+
+            return services;
+        }
+    }
+}
